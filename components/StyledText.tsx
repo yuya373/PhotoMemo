1a445e0d2e7cd4f239e793b9660bcb0109026ac2
南優也 <yuya373@me.com>
2019-01-19 19:19:49 +0900

    initial commit

---
+++ components/StyledText.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+interface Props {
+  style: any,
+}
+
+export class MonoText extends React.Component<Props> {
+  render() {
+    return <Text {...this.props} style={[this.props.style, { fontFamily: 'space-mono' }]} />;
+  }
+}
